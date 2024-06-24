0a9fc034f749a325201308954c5dba21bf4505b0
engnr-junaid <mjunaidahmed469@gmail.com>
2024-06-24 07:34:55 +0500

    Exercise 4 completed

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node *slow = head;
+    node *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;          // move slow pointer one step
+        fast = fast->next->next;    // move fast pointer two steps
+
+        if (slow == fast) {
+            return 1; // Cycle detected
+        }
+    }
+
+    return 0; // No cycle
 }
