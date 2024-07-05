ec567063e72b102e35fac3e37fce11b10283459e
engnr-junaid <mjunaidahmed469@gmail.com>
2024-07-05 14:22:46 +0500

    Lab 04 exercise 4 solved

--- lab04/accumulatortests.s
+++ lab04/accumulatortests.s
@@ -1,10 +1,11 @@
 .import lotsofaccumulators.s
 
 .data
-inputarray: .word 1,2,3,4,5,6,7,0
+#inputarray: .word 1,2,3,4,5,6,7,0
+inputarray: .word 0,1,0,1,0,1,0       #for test 5
 
-TestPassed: .asciiz "Test Passed!"
-TestFailed: .asciiz "Test Failed!"
+TestPassed: .asciiz "Test Passed!\n"
+TestFailed: .asciiz "Test Failed!\n"
 
 .text
 # Tests if the given implementation of accumulate is correct.
@@ -16,9 +17,36 @@ TestFailed: .asciiz "Test Failed!"
 #Modify the test so that you can catch the bugs in four of the five solutions!
 main:
     la a0 inputarray
-    jal accumulatorone
+    addi s0, x0, 3 	#for test 1
     li t0 28
-    beq a0 t0 Pass
+    li t1 3
+    li t2 5		#for test 4
+    li t3 0
+			
+    addi sp sp -16	#for test 2
+    sw t0 0(sp)
+    sw t3 4(sp)
+    sw t2 8(sp)
+    sw t1 12(sp)
+  
+    jal accumulatorone
+    
+  
+    lw t1 12(sp)
+    lw t2 8(sp)
+    lw t3 4(sp)
+    lw t0 0(sp)
+    addi sp sp 16
+    
+    li t4, 5
+    li t5, 0
+    bne t2, t4, Fail  # If t2 is not equal to 5, branch to Fail
+    bne t3, t5, Fail  # If t3 is not equal to 0, branch to Fail
+    
+    bne s0 t1 Fail	#for test 1
+    beq a0 t5 Pass
+    
+     
 Fail:
     la a0 TestFailed
     jal print_string
